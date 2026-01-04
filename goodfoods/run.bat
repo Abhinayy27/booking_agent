01440d18a7bfac73e957e1c0e19b685800d9fc80
Abhinay <bt22cse073@iiitn.ac.in>
2026-01-04 21:46:56 +0530

    Update run.bat

--- goodfoods/run.bat
+++ goodfoods/run.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal
-set OPENAI_API_KEY=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
+set OPENAI_API_KEY=xxxxxxxxxxxxxxxxxxxxxxxxxxx
 streamlit run app.py --server.port 8501 --server.address localhost
 pause
+
 
